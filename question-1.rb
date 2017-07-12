6aa3fcc1fe6d26755b61361dedcb22176f8d3a83
Philipp Fehre <philipp@fehre.co.uk>
2017-07-12 22:03:30 +0100

    refactor to use recursive decent parsing for expression

--- question-1.rb
+++ question-1.rb
@@ -5,34 +5,64 @@
 # - 1 means it is undecidable if the result should be mul or add, therefor we need to backtrack potentially
 
 
-def max_number_rec(prefix_result, nums)
+def eval_factor expr
+  value = expr.shift
+  raise ArgumentError, "#{value} not a number" unless value.is_a? Numeric
+  value
+end
+
+def eval_mul expr
+  factor = eval_factor expr
+
+  while expr.first == :mul
+    expr.shift
+    factor *= eval_factor expr
+  end
+
+  factor
+end
+
+def eval_sum expr
+  sum = eval_mul expr
+
+  while expr.first == :add
+    expr.shift
+    sum += eval_mul expr
+  end
+
+  sum
+end
+
+def eval_expr expr
+  eval_sum expr
+end
+
+def max_number_rec(expr, nums)
   if nums.empty?
-    return eval prefix_result
+    return eval_expr expr
   end
 
   numbers = nums.dup
   current = numbers.shift
-  current_value = eval prefix_result
+  current_value = eval_expr expr.dup
 
   if current_value.zero? || current.zero?
-    max_number_rec("#{prefix_result} + #{current}", numbers)
-  elsif prefix_result == 1
-    max_number_rec("#{prefix_result} + #{current}", numbers)
+    max_number_rec(expr + [:add, current], numbers)
   else
     [
-      max_number_rec("#{prefix_result} + #{current}", numbers),
-      max_number_rec("#{prefix_result} * #{current}", numbers)
+      max_number_rec(expr + [:add, current], numbers),
+      max_number_rec(expr + [:mul, current], numbers)
     ].max
   end
 end
 
 def max_number(s)
-  max_number_rec "0", s.split("").map(&:to_i)
+  max_number_rec [0], s.split("").map(&:to_i)
 end
 
-puts max_number("20")
-puts max_number("02")
-puts max_number("2")
-puts max_number("21")
-puts max_number("214")
-puts max_number("21114")
+p max_number("20")
+p max_number("02")
+p max_number("2")
+p max_number("21")
+p max_number("214")
+p max_number("21114")
