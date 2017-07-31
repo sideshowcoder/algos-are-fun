7f4727d2dbb5de4b75ef3d24880ab317b53400fc
Philipp Fehre <philipp@fehre.co.uk>
2017-07-31 09:28:46 +0100

    Kadanes algorithm

---
+++ kadanes_algorithm.rb
@@ -0,0 +1,17 @@
+# Find the maximum sum subarray
+
+def max_sum(array)
+  current_max = global_max = array.shift
+
+  while n = array.shift
+    current_max = [current_max + n, n].max
+    if current_max > global_max
+      global_max = current_max
+    end
+  end
+
+  global_max
+end
+
+
+p max_sum [1, -1, 3, 1, -2]
